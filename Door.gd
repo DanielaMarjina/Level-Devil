13d36cf8fe2c9a56175b63ad45332edbec242f89
Daniela Marjina <daniela.marjina@student.usv.ro>
2025-12-16 20:19:48 +0200

    Level3, usi capcana

--- Door.gd
+++ Door.gd
@@ -1,7 +1,28 @@
 extends Area2D
 
-export(String, FILE, "*.tscn") var next_scene
+# true = omoară, false = trece mai departe
+export(bool) var is_deadly = false
+export(String) var next_scene = "res://Level4.tscn"
+
+
+onready var other_door = get_parent().get_node("DoorA" if name == "DoorB" else "DoorB")
+
+func _ready():
+	# setează is_deadly din manager
+	if name == "DoorA":
+		is_deadly = DoorManager.door_a_is_deadly
+	else:
+		is_deadly = !DoorManager.door_a_is_deadly
 
 func _on_Door_body_entered(body):
-	if body.name == "Player":
+	if body.name != "Player":
+		return
+
+	if is_deadly:
+		# player moare → swap global + reload
+		DoorManager.swap_doors()
+		print("💀 Ai murit! Ușile s-au schimbat global!")
+		get_tree().reload_current_scene()
+	else:
+		# player trece mai departe
 		get_tree().change_scene(next_scene)
---
+++ DoorManager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var door_a_is_deadly = true
+
+func swap_doors():
+	door_a_is_deadly = !door_a_is_deadly
